1a0cfd17e815fd604ea53835e359a8cc3caf8b36
kumaranish8888 <kumaranish8888@gmail.com>
2017-04-14 19:22:59 -0400

    Added code

---
+++ app.js
@@ -0,0 +1,18 @@
+var foo =  {
+    bar: 1,
+    baz: 2
+}
+
+var {bar, baz} = foo;
+console.log(bar);
+
+var tenses =  ["me", "you", "him"];
+var [ firstperson, secondperson ] = tenses;
+console.log(firstperson);
+
+var name = "Will";
+var age = 34;
+
+
+
+console.log(name);
